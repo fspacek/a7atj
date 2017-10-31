16e19785801589f9e230f14f5b1540db62d62d07
Frantisek Spacek <spacek@devs.eu>
2017-10-31 16:56:25 +0100

    removed sample project from lecture 5

--- lecture_05/blog-app/src/main/resources/META-INF/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO AUTHOR(id, firstname,lastname,email,password) VALUES(hibernate_sequence.nextval,'Jozef', 'Novak', 'jozef@novak.cz','heslo');
-INSERT INTO AUTHOR(id, firstname,lastname,email,password) VALUES(hibernate_sequence.nextval,'Jozef', 'Novak', 'jozef22@novak.cz','heslo');
