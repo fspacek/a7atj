de1278b20d3428ca8680db5f876680f3e8c564d9
Frantisek Spacek <spacek@devs.eu>
2017-11-19 16:29:14 +0100

    removed jsf/jsp blog app

--- lecture_07/blog-app/src/main/resources/META-INF/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO AUTHOR (id, firstname, lastname, password, email) VALUES (hibernate_sequence.nextval, 'Jozef', 'Novak', 'heslo', 'jozef@novak.cz');
-INSERT INTO AUTHOR (id, firstname, lastname, password, email) VALUES (hibernate_sequence.nextval, 'Jozef', 'Novak', 'heslo', 'jozef22@novak.cz');
